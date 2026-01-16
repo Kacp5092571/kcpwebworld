0f511150a6a65cea68d8679d7c6b174624f1654b
Kacp5092571 <kacperkawec481@gmail.com>
2026-01-16 18:24:03 +0000

    Add files via upload

---
+++ userinputtest.js
@@ -0,0 +1,12 @@
+function userinputtest(){
+let a = prompt("What is 12 times 13??")
+let hi = 12
+let hi2 = 13
+let his = hi * hi2
+if (a == his) {
+alert('Correct!');
+}else;{
+let by = his - a
+let Incorrect = "Incorrect by " + by
+alert(Incorrect)}
+}
