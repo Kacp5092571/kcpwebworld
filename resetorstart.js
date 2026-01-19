2001fd7f0502d103d3adee9a10fa1a5da1ad8553
Kacp5092571 <kacperkawec481@gmail.com>
2026-01-19 17:47:14 +0000

    ffgff

--- resetorstart.js
+++ resetorstart.js
@@ -4,6 +4,6 @@ function resetorstart(){
 function click(){
     let clicks = clicks+1;
 }
-while (click > 0){
-document.getElementById("score").textContent (click;)
+while (clicks > 0){
+document.getElementById("score").innerHTML = clicks;
 }
